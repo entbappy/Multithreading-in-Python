990233a7b7ce5e2fd776f22f1786968f726ee091
entbappy <entbappy73@gmail.com>
2022-06-21 16:30:12 +0600

    added

--- app.py
+++ app.py
@@ -1,8 +1,43 @@
 from flask import Flask, render_template, request, jsonify
 import time
+import threading
 
 
 app = Flask(__name__)
+
+
+
+def trainModel():
+    print("Loading data..")
+
+
+def loadData():
+    time.sleep(30)
+    print("Load..")
+
+class TrainModel (threading.Thread):
+    def __init__(self, data):
+        threading.Thread.__init__(self)
+        self.data = data
+    def run(self):
+        # return model loss
+        self._return = trainModel()    
+    def join(self):
+        threading.Thread.join(self)
+        return self._return
+
+
+class LoadData (threading.Thread):
+    def __init__(self, filenames):
+        threading.Thread.__init__(self)
+        self.filenames = filenames
+    def run(self):        
+        # return data
+        self._return = loadData()
+    def join(self):
+        threading.Thread.join(self)
+        return self._return
+
 
 
 @app.route('/', methods=['GET', 'POST']) # To render Homepage
@@ -13,8 +48,20 @@ def home_page():
 @app.route('/train', methods=['POST'])  # This will be called from UI
 def train_model():
     if (request.method=='POST'):
-        time.sleep(10)
-        return jsonify({'status': 'success'})
+          for _ in range(10):
+            # loadData
+            loadThread = LoadData(None)
+            loadThread.start()
+            
+            # trainModel
+            trainThread = TrainModel(None)
+            trainThread.start()
+            
+            # only continue if both threads are done
+            modelLoss = trainThread.join()
+            data = loadThread.join()
+
+            return jsonify({'status': 'success'})
 
 
 
